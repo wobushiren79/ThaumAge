d3c2bb767db0d4e23c15f875aa7358c01aeca935
Joker Apple <734719526@qq.com>
2021-03-24 12:47:19 +0800

    test

--- ThaumAge/Assets/Scrpits/Test/Test.cs
+++ ThaumAge/Assets/Scrpits/Test/Test.cs
@@ -1,44 +1,26 @@
-﻿using UnityEngine;
+﻿using Pathfinding;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class Test : BaseMonoBehaviour
 {
+    public AIPath aiPath;
 
-    CharacterController cc;
-    public float speed = 20;
-    public float viewRange = 30;
-    public Chunk chunkPrefab;
-
+    public Transform targetPosition;
     private void Start()
     {
-        cc = GetComponent<CharacterController>();
+        AstarPath.active.Scan();
+        aiPath.isStopped = false;
+        aiPath.destination = targetPosition.position;
+        aiPath.SearchPath();
+        aiPath.onSearchPath = () =>
+        {
+            aiPath.canMove = true;
+        };
     }
 
     void Update()
     {
-        UpdateInput();
     }
-
-    void UpdateInput()
-    {
-        var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
-
-        var x = Input.GetAxis("Mouse X");
-        var y = Input.GetAxis("Mouse Y");
-
-        transform.rotation *= Quaternion.Euler(0f, x, 0f);
-        transform.rotation *= Quaternion.Euler(-y, 0f, 0f);
-
-        if (Input.GetButton("Jump"))
-        {
-            cc.Move((transform.right * h + transform.forward * v + transform.up) * speed * Time.deltaTime);
-        }
-        else
-        {
-            cc.SimpleMove(transform.right * h + transform.forward * v * speed);
-        }
-    }
-
 
 }
