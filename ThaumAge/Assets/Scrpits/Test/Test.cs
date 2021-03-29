ba8d7faf543bcc7be5de8ec32a7465ef1c35e581
Joker Apple <734719526@qq.com>
2021-03-29 18:48:20 +0800

    test

--- ThaumAge/Assets/Scrpits/Test/Test.cs
+++ ThaumAge/Assets/Scrpits/Test/Test.cs
@@ -7,11 +7,7 @@ public class Test : BaseMonoBehaviour
 {
     public void Start()
     {
-        Vector3Int pos = new Vector3Int(-, 0, -8);
-        int posX = (int)Mathf.Floor(pos.x / 16f);
 
-        int posZ = (int)Mathf.Floor(pos.z / 16f);
-        LogUtil.Log(posX + " " + posZ);
     }
     
 }
