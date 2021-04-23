e2f9e150d30ca9784e0086d98a7f47b55b0935ca
Joker Apple <734719526@qq.com>
2021-04-23 18:19:21 +0800

    更改

--- ThaumAge/Assets/Scrpits/Game/Biome/BiomeMagicForest.cs
+++ ThaumAge/Assets/Scrpits/Game/Biome/BiomeMagicForest.cs
@@ -11,6 +11,30 @@ public class BiomeMagicForest : Biome
     public override BlockTypeEnum GetBlockType(int genHeight, Vector3Int localPos, Vector3Int wPos)
     {
         base.GetBlockType(genHeight, localPos, wPos);
-        return BlockTypeEnum.Dirt;
+        if (wPos.y == genHeight)
+        {
+            //AddWeed(wPos);
+            //AddFlower(wPos);
+            //AddTree(wPos);
+            //AddBigTree(wPos);
+            //AddWorldTree(wPos);
+            // 地表，使用草
+            return BlockTypeEnum.Grass;
+        }
+        if (wPos.y < genHeight && wPos.y > genHeight - 10)
+        {
+            //中使用泥土
+            return BlockTypeEnum.Dirt;
+        }
+        else if (wPos.y == 0)
+        {
+            //基础
+            return BlockTypeEnum.Foundation;
+        }
+        else
+        {
+            //其他石头
+            return BlockTypeEnum.Stone;
+        }
     }
 }
