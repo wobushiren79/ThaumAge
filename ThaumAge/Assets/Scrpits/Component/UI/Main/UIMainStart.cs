89ffc6761af6c759c19d06188be6da9066a29157
ZhongMing <734719526@qq.com>
2021-08-23 19:12:29 +0800

    框架修改

--- ThaumAge/Assets/Scrpits/Component/UI/Main/UIMainStart.cs
+++ ThaumAge/Assets/Scrpits/Component/UI/Main/UIMainStart.cs
@@ -5,8 +5,57 @@ using UnityEngine.UI;
 public partial class UIMainStart : BaseUIComponent
 {
 
+    public void Start()
+    {
+        
+    }
+
     public override void OnClickForButton(Button viewButton)
     {
         base.OnClickForButton(viewButton);
+        if (viewButton == ui_Create) HandleForCreate();
+        else if (viewButton == ui_Continue) HandleForContinue();
+        else if (viewButton == ui_Setting) HandleForSetting();
+        else if (viewButton == ui_Exit) HandleForExit();
+    }
+
+    /// <summary>
+    /// 设置UI文本
+    /// </summary>
+    public void SetUIText()
+    {
+
+    }
+
+    /// <summary>
+    /// 处理-创建游戏
+    /// </summary>
+    public void HandleForCreate()
+    {
+
+    }
+
+    /// <summary>
+    /// 处理-继续游戏
+    /// </summary>
+    public void HandleForContinue()
+    {
+
+    }
+
+    /// <summary>
+    /// 游戏设置
+    /// </summary>
+    public void HandleForSetting()
+    {
+
+    }
+
+    /// <summary>
+    /// 处理-离开游戏
+    /// </summary>
+    public void HandleForExit()
+    {
+        GameUtil.ExitGame();
     }
 }
