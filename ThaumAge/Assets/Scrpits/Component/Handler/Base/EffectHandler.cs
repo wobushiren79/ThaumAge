0f9bf1d9130fc30a31de4ad23e21355b28d506b0
ZhongMing <734719526@qq.com>
2021-10-22 18:42:28 +0800

    添加粒子特效

--- ThaumAge/Assets/Scrpits/Component/Handler/Base/EffectHandler.cs
+++ ThaumAge/Assets/Scrpits/Component/Handler/Base/EffectHandler.cs
@@ -5,55 +5,25 @@ using System;
 
 public class EffectHandler : BaseHandler<EffectHandler, EffectManager>
 {
-
     /// <summary>
-    /// 播放粒子特效
+    /// 展示粒子特效
     /// </summary>
-    /// <param name="name"></param>
-    /// <param name="effectPosition"></param>
-    public void PlayEffect(GameObject objContainer, string name, Vector3 effectPosition, float delayTime, Action<GameObject> callBack = null)
+    /// <param name="objContainer"></param>
+    /// <param name="effectData"></param>
+    /// <param name="callBack"></param>
+    public void ShowEffect(GameObject objContainer,EffectBean effectData,Action<GameObject> callBack = null)
     {
-        manager.CreateEffect(objContainer, name, (objEffect) =>
+        manager.CreateEffect(objContainer, effectData, (objEffect)=> 
         {
-            if (objEffect == null)
-            {
-                callBack?.Invoke(null);
-                return;
-            }
-            objEffect.transform.position = effectPosition;
-            ParticleSystem[] listParticleSystem = objEffect.GetComponentsInChildren<ParticleSystem>();
-            for (int i = 0; i < listParticleSystem.Length; i++)
-            {
-                ParticleSystem particleSystem = listParticleSystem[i];
-                ParticleSystem.MainModule psMain = particleSystem.main;
-                psMain.loop = false;
-                //psMain.stopAction = ParticleSystemStopAction.Callback;
-                particleSystem.Play();
-            }
-            if (delayTime <= 0)
-            {
-                callBack?.Invoke(objEffect);
-                return;
-            }
-            StartCoroutine(CoroutineForDelayDestroy(objEffect, delayTime));
             callBack?.Invoke(objEffect);
+            if(effectData.timeForShow > 0) 
+            {
+                //展示时间过后就删除
+                this.WaitExecuteSeconds(effectData.timeForShow, () =>
+                {
+                    Destroy(objEffect);
+                });
+            }
         });
     }
-
-    public void PlayEffect(string name, Vector3 effectPosition, float delayTime, Action<GameObject> callBack = null)
-    {
-        PlayEffect(null, name, effectPosition, delayTime, callBack);
-    }
-
-    public void PlayEffect(string name, Vector3 effectPosition, Action<GameObject> callBack = null)
-    {
-        PlayEffect(name, effectPosition, 5, callBack);
-    }
-
-    public IEnumerator CoroutineForDelayDestroy(GameObject objEffect, float delayTime)
-    {
-        yield return new WaitForSeconds(delayTime);
-        Destroy(objEffect);
-    }
-
 }
