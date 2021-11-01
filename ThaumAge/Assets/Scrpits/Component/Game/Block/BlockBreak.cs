e09e8a83cca2a6e66aca05a7538a36935bfbe535
ZhongMing <734719526@qq.com>
2021-11-01 18:07:00 +0800

    修改

--- ThaumAge/Assets/Scrpits/Component/Game/Block/BlockBreak.cs
+++ ThaumAge/Assets/Scrpits/Component/Game/Block/BlockBreak.cs
@@ -3,5 +3,14 @@ using UnityEngine;
 
 public class BlockBreak : BaseMonoBehaviour
 {
-    
+    public MeshRenderer mrBlockBreak;
+
+    /// <summary>
+    /// 设置破碎进度
+    /// </summary>
+    /// <param name="pro"></param>
+    public void SetBreakPro(float pro)
+    {
+
+    }
 }
