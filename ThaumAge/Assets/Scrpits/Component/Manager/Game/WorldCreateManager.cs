507b1e4a471f4f80228bbfa953cfac1707d0d3fb
Apple <734719526@qq.com>
2021-11-15 23:18:16 +0800

    优化

--- ThaumAge/Assets/Scrpits/Component/Manager/Game/WorldCreateManager.cs
+++ ThaumAge/Assets/Scrpits/Component/Manager/Game/WorldCreateManager.cs
@@ -25,7 +25,7 @@ public class WorldCreateManager : BaseManager
     protected int worldSeed;
 
     public int widthChunk = 16;
-    public int heightChunk = 255;
+    public int heightChunk = 256;
     //世界大小
     public int worldSize = 10000;
     //世界范围
