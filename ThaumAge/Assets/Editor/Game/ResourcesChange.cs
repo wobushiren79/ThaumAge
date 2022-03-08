72651dff4840076bfcc85337cb2f7a7082054c52
ZhongMing <734719526@qq.com>
2022-03-08 18:44:48 +0800

    资源修改

---
+++ ThaumAge/Assets/Editor/Game/ResourcesChange.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ResourcesChange : AssetPostprocessor
+{
+    public void OnPostprocessPrefab(GameObject changeObj)
+    {
+        Debug.Log("changeObj:"+ changeObj.name);
+    }
+}
