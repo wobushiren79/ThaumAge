eae3d56df7daa6089c1332d72f187157c5cbf7a5
Apple\AppleCoffee <734719526@qq.com>
2022-08-18 22:42:35 +0800

    时间倍数调整

--- ThaumAge/Assets/Scrpits/Component/Handler/Game/GameTimeHandler.cs
+++ ThaumAge/Assets/Scrpits/Component/Handler/Game/GameTimeHandler.cs
@@ -13,7 +13,7 @@ public class GameTimeHandler : BaseHandler<GameTimeHandler, GameTimeManager>
         GameStateEnum gameState = GameHandler.Instance.manager.GetGameState();          
         Time.timeScale = timeScale;
         //受缩放时间影响
-        if (timeOffset >= 5)
+        if (timeOffset >= 2)
         {
             switch (gameState) 
             {
