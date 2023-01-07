b86bd9ebf78f79a10a6cf9abe0f5a9816aeb5067
Apple\AppleCoffee <734719526@qq.com>
2023-01-07 19:28:09 +0800

    罐子液体添加

---
+++ ThaumAge/Assets/Scrpits/Game/Block/Meta/BlockMetaWardedJar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BlockMetaWardedJar : BlockMetaBase
+{
+    public int elementalType = 1;
+    public int curElemental = 0;
+    public int maxElemental = 100;
+    
+    /// <summary>
+    /// 获取元素类型
+    /// </summary>
+    /// <returns></returns>
+    public ElementalTypeEnum GetElementalType()
+    {
+        return (ElementalTypeEnum)elementalType;
+    }
+
+    /// <summary>
+    /// 获取元素进度
+    /// </summary>
+    /// <returns></returns>
+    public float GetElementalPro()
+    {
+        if (maxElemental == 0)
+            return 0;
+        return curElemental / (float)maxElemental;
+    }
+}
---
+++ ThaumAge/Assets/Scrpits/Game/Block/Types/BlockTypeWardedJar.cs
@@ -0,0 +1,89 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BlockTypeWardedJar : Block
+{
+    public override void CreateBlockModelSuccess(Chunk chunk, Vector3Int localPosition, BlockDirectionEnum blockDirection, GameObject obj)
+    {
+        base.CreateBlockModelSuccess(chunk, localPosition, blockDirection, obj);
+        BlockBean blockData = chunk.GetBlockData(localPosition);
+        BlockMetaWardedJar blockMetaWardedJar = null;
+        if (blockData == null)
+        {
+
+        }
+        else
+        {
+            blockMetaWardedJar = blockData.GetBlockMeta<BlockMetaWardedJar>();
+        }
+        if (blockMetaWardedJar == null)
+        {
+            blockMetaWardedJar = new BlockMetaWardedJar();
+        }
+        SetLiquidPro(chunk, localPosition, blockMetaWardedJar);
+    }
+
+    /// <summary>
+    /// 设置液体进度
+    /// </summary>
+    public void SetLiquidPro(Chunk chunk, Vector3Int localPosition, BlockMetaWardedJar blockMetaWardedJar) 
+    {
+        SetLiquidPro(chunk, localPosition, blockMetaWardedJar.GetElementalType(), blockMetaWardedJar.GetElementalPro());
+    }
+
+    /// <summary>
+    /// 设置液体进度
+    /// </summary>
+    public void SetLiquidPro(Chunk chunk, Vector3Int localPosition,ElementalTypeEnum elementalType, float liquidPro)
+    {
+        GameObject objBlock = chunk.GetBlockObjForLocal(localPosition);
+        if (objBlock == null)
+            return;
+        Transform tfLiquid = objBlock.transform.Find("Liquid");
+        if (liquidPro == 0)
+        {
+            tfLiquid.ShowObj(false);
+        }
+        else
+        {
+            tfLiquid.ShowObj(true);
+            var elementalInfo =  ElementalInfoCfg.GetItemData(elementalType);
+            ColorUtility.TryParseHtmlString($"{elementalInfo.color}", out Color elementalColor);
+
+            MeshRenderer meshRenderer = tfLiquid.GetComponent<MeshRenderer>();
+            meshRenderer.material.SetFloat("_Fill", liquidPro - 0.5f);
+            meshRenderer.material.SetFloat("_WobbleX", 0);
+            meshRenderer.material.SetFloat("_WobbleZ", 0);
+            meshRenderer.material.SetFloat("_Emission", 0f);
+            meshRenderer.material.SetFloat("_FresnelPower", 0);
+            meshRenderer.material.SetColor("_LiquidColor",elementalColor);
+            meshRenderer.material.SetColor("_TopColor", elementalColor * 0.9f);
+        }
+    }
+
+    //public override bool TargetUseBlock(GameObject user, ItemsBean itemData, Chunk targetChunk, Vector3Int targetWorldPosition)
+    //{
+    //    BlockBean blockData = targetChunk.GetBlockData(targetWorldPosition - targetChunk.chunkData.positionForWorld);
+    //    BlockMetaWardedJar blockMetaWardedJar = null;
+    //    if (blockData == null)
+    //    {
+
+    //    }
+    //    else
+    //    {
+    //        blockMetaWardedJar = blockData.GetBlockMeta<BlockMetaWardedJar>();
+    //    }
+    //    if (blockMetaWardedJar == null)
+    //    {
+    //        blockMetaWardedJar = new BlockMetaWardedJar();
+    //    }
+    //    blockMetaWardedJar.curElemental += 10;
+    //    blockData.SetBlockMeta(blockMetaWardedJar);
+    //    targetChunk.SetBlockData(blockData);
+
+    //    SetLiquidPro(targetChunk, targetWorldPosition - targetChunk.chunkData.positionForWorld, blockMetaWardedJar);
+
+    //    return base.TargetUseBlock(user, itemData, targetChunk, targetWorldPosition);
+    //}
+
+}
