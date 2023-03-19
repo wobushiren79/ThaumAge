7839f5e758c8aee8b4203a79e86dff7b49759eaf
Apple\AppleCoffee <734719526@qq.com>
2023-03-19 18:33:43 +0800

    设置提交

---
+++ ThaumAge/Assets/Scrpits/Bean/Game/GameConfigBeanPartial.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class GameConfigBean
+{
+
+    //角色X轴镜头移动速度
+    public float speedForPlayerCameraMoveX = 2f;
+    //角色Y轴镜头移动速度
+    public float speedForPlayerCameraMoveY = 2f;
+
+    //世界刷新范围
+    public int worldRefreshRange = 5;
+    //世界删除范围
+    public int worldDestoryRange = 5;
+}
